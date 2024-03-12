d7f5a20b247bbacfb521fa52b45e21a33064674e
Sakshi <iamsakshiingole@gmail.com>
2024-03-12 14:35:29 -0400

    File added

--- Image Processing/1_DisplayImg.py
+++ Image Processing/1_DisplayImg.py
@@ -2,5 +2,6 @@ import cv2
 print("Package imported")
 
 img = cv2.imread("D:/My codes/Computer Vision/Image Processing/Resources/Nature.jpg")
-cv2.imshow("Output", img)
+imgResize = cv2.resize(img, (640, 480))
+cv2.imshow("Output", imgResize)
 cv2.waitKey(0)
---
+++ Image Processing/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+img = cv2.imread("D:/My codes/Computer Vision/Image Processing/Resources/Nature.jpg")
+cv2.imshow("Output", img)
+cv2.waitKey(0)
