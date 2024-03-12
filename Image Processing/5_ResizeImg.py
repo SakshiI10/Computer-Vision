62ba8855eef82e2665c1d59a94aab3e3c3fb7880
Sakshi <iamsakshiingole@gmail.com>
2024-03-12 14:46:53 -0400

    File added

---
+++ Image Processing/5_ResizeImg.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("D:/My codes/Computer Vision/Image Processing/Resources/Gateway.jpg")
+
+print(img.shape)
+# (3174, 3000, 3): 3174: height, 3000: width, 3: BGR(No of channels)
+# To resize the image pixels
+imgResize = cv2.resize(img, (300, 200))
+print(imgResize.shape)
+
+cv2.imshow("Image", img)
+cv2.imshow("Resized Image", imgResize)
+cv2.waitKey(0)
