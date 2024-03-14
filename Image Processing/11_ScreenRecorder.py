650856b12544238989de017632a300ee9a275871
Sakshi <iamsakshiingole@gmail.com>
2024-03-14 16:38:19 -0400

    File added

---
+++ Image Processing/11_ScreenRecorder.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 16:18:02 2024
+
+@author: SMI
+"""
+
+import pyautogui as p
+import cv2 as c
+import numpy as np
+
+#Create resolution
+rs = p.size()
+
+#filename in which we store recording
+fn = input("Please Enter any file name and Path")
+#Fix the frame rate
+fps = 60.0
+fourcc = c.VideoWriter_fourcc(*'XVID')
+output = c.VideoWriter(fn,fourcc,fps,rs)
+
+#create recording module
+c.namedWindow("Live_Recording",c.WINDOW_NORMAL)
+
+#Resize the window
+c.resizeWindow("Live_Recording",(600,400))
+
+while True:
+    img = p.screenshot() #image
+    f = np.array(img) #convert image into array
+    f = c.cvtColor(f,c.COLOR_BGR2RGB)
+    output.write(f)
+    c.imshow("Live_Recording", f)
+    if c.waitKey(1) == ord("q"):
+        break
+
+
+output.release() 
+c.destroyAllWindows() 
