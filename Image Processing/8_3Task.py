fd7ad99e323e6f84a0bc1b70fe7af3cc7ca5e5e9
Sakshi <iamsakshiingole@gmail.com>
2024-03-14 12:53:24 -0400

    File added

---
+++ Image Processing/8_3Task.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 12:31:18 2024
+
+@author: SMI
+"""
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+print(cap)
+
+#DIVX, XVID, MJPG, X264, WMV1, WMV2
+fourcc = cv2.VideoWriter_fourcc(*"XVID")  # *"XVID"
+#It contain 4 parameter , name, codec,fps,resolution
+output = cv2.VideoWriter("D:\\My codes\\Computer Vision\\Image Processing\\Resources\\output.avi",fourcc,20.0,(640,480),0)
+
+while(cap.isOpened()):
+    #here read the vdo
+    ret, vdo = cap.read()  
+    
+    if ret==True:
+        gray  = cv2.cvtColor(vdo,cv2.COLOR_BGR2GRAY)
+        #here flip is used to lip the video at recording time
+        vdo = cv2.flip(vdo,1)
+        gray = cv2.flip(gray,1)
+        cv2.imshow("Gray Frame",gray)
+        cv2.imshow('Colorframe',vdo)
+        output.write(gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):   #press to exit
+            break
+        
+cap.release()
+output.release()
+cv2.destroyAllWindows()
