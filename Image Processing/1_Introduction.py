2a31882bb6f963c509c1b32038b7676fb3f6393f
Sakshi <iamsakshiingole@gmail.com>
2024-03-13 22:50:19 -0400

    File added

---
+++ Image Processing/1_Introduction.py
@@ -0,0 +1,13 @@
+''' 
+Computer Vision
+a) Pixel
+b) Images
+c) Videos
+d) Image Processing
+e) Computer Vision
+f) Open CV
+
+Images are of 2 types:
+a) Color: 3-Channel
+b) Gray: Single Channel
+'''
