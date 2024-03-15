3e7faeab38b6ce6131f537f33e7e182f38072c52
Sakshi <iamsakshiingole@gmail.com>
2024-03-14 22:42:40 -0400

    File added

---
+++ Image Processing/20_ImageOperations.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 22:21:22 2024
+
+@author: SMI
+"""
+import cv2
+import numpy as np
+
+img=cv2.imread("D:\\My codes\\Computer Vision\\Image Processing\\Resources\\pikachu.jpg")
+img = cv2.resize(img,(300,300))
+
+#returns a tuple of number of rows, columns, and channels
+print("shape==",img.shape) 
+
+#returns Total number of pixels is accessed
+print("no.of pixels==",img.size) 
+
+#returns Image datatype is obtained
+print("datatype==",img.dtype) 
+
+print("Imagetype==",type(img))
+#cv2.imshow("res",img)
+
+#split  -  return 3 channel of ur image which is blue,green,red
+print(cv2.split(img))
+b,g,r = cv2.split(img)
+'''
+cv2.imshow("blue",b)
+cv2.imshow("green",g)
+cv2.imshow("red",r)
+cv2.imshow("Original",img)'''
+
+#To mix the the channels then use merge
+cv2.imshow("Original",img)
+mr1 = cv2.merge((r,g,b))
+cv2.imshow("rgb",mr1)
+
+mr2 = cv2.merge((g,b,r))
+cv2.imshow("gbr",mr2)
+
+mr2 = cv2.merge((b,g,r))
+cv2.imshow("bgr",mr2)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
---
+++ Image Processing/21_ColorAtCoordinates.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 22:36:43 2024
+
+@author: SMI
+"""
+import cv2
+
+img = cv2.imread("D:\\My codes\\Computer Vision\\Image Processing\\Resources\\lion.jpg")
+img = cv2.resize(img,(600,600))
+cv2.imshow("lion",img)
+
+#access a pixel value by its row and column coordinates.
+px = img[520,580] #store cordinate in variable
+print("The pixel of that co-ordinates: ",px) #we get the pixel value
+
+#Now try to find selected channel value from cordinate
+#We know we have three channel -   0,1,2
+# accessing only blue pixel
+blue = img[520,580,0]
+print("the pixel having blue color==",blue)
+
+grn = img[520,580,1] #for green pass 1
+print("the pixel having grn color==",grn)
+
+red = img[520,580,2] #for red pass 2
+print("the pixel having red color==",red)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
