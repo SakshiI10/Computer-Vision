45b19fbb61b7cd89f6882e6d4680afd7d971900d
Sakshi <iamsakshiingole@gmail.com>
2024-03-31 23:09:33 -0400

    File added

--- Assignment/2_Assg.py
+++ Assignment/2_Assg.py
@@ -8,12 +8,17 @@ def draw(event, x, y, flag, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         #cordinates = ". "+ str(x) + ', '+ str(y)
         #cv2.putText(img, cord, (x, y), cv2.FONT_HERSHEY_PLAIN, 1, (155,125 ,100), 2)
-        cv2.rectangle(img,(x,y),(512,512),(0,0,255),1)
+        cv2.rectangle(img,(x,y),(512, 512),(0,0,255),1)
+        cv2.rectangle(img,(0,0),(x,y),(0,0,255),1)
         
         w = 512 - x
         h = 512 - y
-        area = 2 * (w + h)
-        print("Area:", area)
+        area1 = 2 * (x+y)
+        area2 = 2 * (w + h)
+        #print("Area:", area1)
+        #print("Area:", area2)
+        MaxArea=max(area1, area2)
+        print("MaxArea: ", MaxArea)
 
 cv2.namedWindow(winname = "Result")
 img = np.zeros((512,512,3), np.uint8)
